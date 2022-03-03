0a0e3874a0b6478fdad61e9a2bd168add43d9c5c
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-03 16:52:20 +0600

    add

--- js/node.js
+++ js/node.js
@@ -1,3 +1,28 @@
+const getMobileApi = () => {
+  fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    .then((res) => res.json())
+    .then((data) => addMobileApi(data.data.slice(0, 6)));
+};
+getMobileApi();
+const addMobileApi = (mobileApis) => {
+  console.log(mobileApis);
+  mobileApis.forEach((mobileApi) => {
+    const addMobile = document.getElementById("addMobile");
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="col ">
+    <div class="card h-75 border-0 ">
+      <div class="d-flex justify-content-center">
+          <img src="${mobileApi.image}" class="text-center w-50 card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title text-center">${mobileApi.phone_name}</h5>
+      </div>
+    </div>
+  </div>`;
+    addMobile.appendChild(div);
+  });
+};
+
 const getValue = () => {
   const error = document.getElementById("error");
   const error1 = document.getElementById("error1");
@@ -37,6 +62,7 @@ const getValue = () => {
     // spinner
     if ((onclick = "getValue()")) {
       document.getElementById("spinner").style.display = "block";
+      document.getElementById("hideCarousel").style.display = "none";
     }
     error.innerText = "";
   }
